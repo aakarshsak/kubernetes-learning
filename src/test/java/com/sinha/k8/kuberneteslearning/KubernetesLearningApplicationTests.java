171feca8b4ece753cf41ec8f370db84149f39665
Aakarsh Sinha <aakarshsak@gmail.com>
2023-10-28 20:41:47 +0530

    Initializing

---
+++ src/test/java/com/sinha/k8/kuberneteslearning/KubernetesLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinha.k8.kuberneteslearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
